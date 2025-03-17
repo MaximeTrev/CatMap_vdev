978917a8435aa0de775bd584975ef1bf0439817b
MaximeTrev <maxime.trevisani@square-management.com>
2025-03-17 09:19:04 +0100

    Ajout du dossier Overpass

---
+++ Overpass/CSS.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+###################################################### CSS #################
+def __CssClassPopup():
+    popup_class = "font-family:sans-serif; \
+        font-size:10px;  \
+        color:grey; \
+        border-radius: 5px; \
+        padding: 1px 1px 1px 1px;"
+    return popup_class
