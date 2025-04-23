efc0fc9b3f69cd6aaf3a30e9b29e70d32a386045
MaximeTrev <maxime.trevisani@square-management.com>
2025-04-23 15:18:25 +0200

    Update name_to_georef.py

--- Overpass/name_to_georef.py
+++ Overpass/name_to_georef.py
@@ -117,7 +117,7 @@ def __var_name__(name, booleen = False): #sous-fonction
         variations.append((name.capitalize(), 3)) #Xxx
 
     #Suppression des doublons avec le Flag 0, et si doublon on garde le Flag 0
-    var_noduplicata[variations[0]] # Référence (flag 0)
+    var_noduplicata = [variations[0]] # Référence (flag 0)
     st.write("ini")
     st.write(var_noduplicata)
     for name, flag in variations:
