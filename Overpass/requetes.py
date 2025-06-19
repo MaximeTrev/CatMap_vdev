e93ba213f2fd2b2cabd99ee58e2c894d8242c0f2
MaximeTrev <maxime.trevisani@square-management.com>
2025-06-19 12:00:55 +0200

    Update requetes.py

--- Overpass/requetes.py
+++ Overpass/requetes.py
@@ -12,7 +12,7 @@ def get_overpass_data(company_name):
     
     """
     api = overpy.Overpass()
-    query = f"""[out:json][timeout:180];(node["name"="{company_name}"];way["name"="{company_name}"];);out center;"""
+    query = f"""[out:json][timeout:180];(node["name"~"{company_name}"];way["name"~"{company_name}"];);out center;"""
     # Ajout de "out center;" pour forcer le centre des ways et relations
     try:
         result = api.query(query)
