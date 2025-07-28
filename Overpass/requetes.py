d3f1d6cb2127885ef28c03edd12af4e8169d7418
MaximeTrev <maxime.trevisani@square-management.com>
2025-07-28 09:47:56 +0200

    Update requetes.py

--- Overpass/requetes.py
+++ Overpass/requetes.py
@@ -62,8 +62,8 @@ def get_overpass_data(company_name):
     query = f"""
     [out:json][timeout:180];
     (
-      node["name"{regex_operator}"{regex}([\\s\\-_])",i][!"highway"][!"place"][!"junction"];
-      way["name"{regex_operator}"{regex}([\\s\\-_])",i][!"highway"][!"place"][!"junction"];
+      node["name"{regex_operator}"{regex}\\\\b",i][!"highway"][!"place"][!"junction"];
+      way["name"{regex_operator}"{regex}\\\\b",i][!"highway"][!"place"][!"junction"];
     );
     out center;
     """ 
